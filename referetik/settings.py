95c5e97212d86e7b1f6a1ef96a68e3bcd77e0c62
Muhammadyorkhon <48237769+Dior1726@users.noreply.github.com>
2021-02-15 16:43:28 +0600

    'deploy'

--- referetik/settings.py
+++ referetik/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,7 +127,6 @@ STATEFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
-import dj_database_url
 
-db_from_env = dj_database_url.url.config()
+db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
